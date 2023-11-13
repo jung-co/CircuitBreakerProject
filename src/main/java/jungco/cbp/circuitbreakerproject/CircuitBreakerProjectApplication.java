d25bd1ddcc7931ebbf7b32779ab7b159d8dc6510
jung-co <dal_a2@naver.com>
2023-11-13 22:34:02 +0900

    init commit

---
+++ src/main/java/jungco/cbp/circuitbreakerproject/CircuitBreakerProjectApplication.java
@@ -0,0 +1,13 @@
+package jungco.cbp.circuitbreakerproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuitBreakerProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CircuitBreakerProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jungco/cbp/circuitbreakerproject/CircuitBreakerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package jungco.cbp.circuitbreakerproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitBreakerProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
